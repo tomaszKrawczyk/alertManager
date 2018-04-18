03e18e498f671e6c375dcaf731302bec21cc760f
Tomasz Krawczyk <tom_krawczyk@op.pl>
2018-04-18 20:49:39 +0200

    change scobe to session

--- src/main/java/pl/tomaszKrawczyk/allertManager/model/services/AlertListService.java
+++ src/main/java/pl/tomaszKrawczyk/allertManager/model/services/AlertListService.java
@@ -1,5 +1,7 @@
 package pl.tomaszKrawczyk.allertManager.model.services;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import pl.tomaszKrawczyk.allertManager.model.AlertForm;
 
@@ -10,7 +12,8 @@ import java.util.List;
  * Created by Tomek Krawczyk on 24.02.2018.
  */
 
-@Service
+@Service()
+@Scope(value = "session",proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class AlertListService {
 
     private List<AlertForm> alertFormList;
